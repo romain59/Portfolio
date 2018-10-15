13ccfe7faf7d106ea156d9b1ec62fe83f4ffa06d
romain59 <43751417+romain59@users.noreply.github.com>
2018-10-15 16:48:31 +0200

    Portfolio1

---
+++ js.js
@@ -0,0 +1,23 @@
+
+/*
+function valider() {
+     // si la valeur du champ nom est non vide
+    if (
+
+        document.formSaisie.name.value != "" &&
+        document.formSaisie.prenom.value != "" &&
+        document.formSaisie.email.value != "" &&
+        document.formSaisie.message.value != "")
+    {
+        // les données sont ok, on peut envoyer le formulaire
+        return true;
+    }
+    else {
+        // sinon on affiche un message
+        alert("Désole le formulaire est mal remplie");
+        // et on indique de ne pas envoyer le formulaire
+        return false;
+    }
+}
+
+*/
